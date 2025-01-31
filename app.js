9f01c531389c3f0656c7e610bb081eace8545c1e
Coba Paz <cobajayy@gmail.com>
2025-01-30 22:02:17 -0700

    1.30.25 html, css, js update

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 console.log("connected to memory game")
+// 
+/*-------------------------------- Constants --------------------------------*/
+
+let timer;
+let gameOver;
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+
+/*------------------------ Cached Element References ------------------------*/
+const boardElement = document.getElementsByClassName("board")
+const cardsElement = document.querySelectorAll(".card")
+
+const messageElement = document.getElementById("message")
+const playAgainElement = document.getElementById("play-again")
+
+console.dir(cardsElement)
+/*-------------------------------- Functions --------------------------------*/
+const init = () => {
+
+}
+
+flipCard = () => {
+    // cardsElement.hasAttribute("disabled");
+
+    console.log("i'm clicked")
+}
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+cardsElement.forEach(card => card.addEventListener("click", flipCard));
+playAgainElement.addEventListener("click", init)
